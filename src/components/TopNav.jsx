bfe9054a291facc9512cfe58653cf5303699f3be
MilosGlamocak <milosglamocak@gmail.com>
2024-09-18 16:45:24 +0200

    topnav scroll anim

--- src/components/TopNav.jsx
+++ src/components/TopNav.jsx
@@ -1,45 +1,67 @@
-import React, { useRef, useState } from 'react'
-import '../styles/TopNav.scss'
+import React, { useRef, useState, useEffect } from 'react';
+import '../styles/TopNav.scss';
 import MenuIcon from '@mui/icons-material/Menu';
 import NavWindowMobile from './NavWindowMobile';
 
-
 function TopNav() {
-    let [open, setOpen] = useState(false);
+    const [open, setOpen] = useState(false);
+    const [topStyle, setTopStyle] = useState('0rem');
     const navRef = useRef(null);
+    const lastScrollTop = useRef(0);
+    const tolerance = 1; // Tolerance threshold in pixels
+    const viewThreshold = window.innerHeight * 0.3; // 30vh in pixels
 
     const handleOpen = () => {
         if (open) {
             navRef.current.style.animation = '0.2s closeNav forwards ease';
             setTimeout(() => {
                 setOpen(!open);
-                document.body.style.overflow = ''; // Restore scrolling
+                document.body.style.overflow = '';
             }, 200);
         } else {
             setOpen(!open);
-            document.body.style.overflow = 'hidden'; // Disable scrolling
+            document.body.style.overflow = 'hidden';
         }
     };
-    
-    
+
+    useEffect(() => {
+        const handleScroll = () => {
+            const currentScrollTop = window.pageYOffset || document.documentElement.scrollTop;
+
+            if (currentScrollTop <= viewThreshold) {
+                // Always visible if within the first 30vh
+                setTopStyle('0rem');
+            } else if (currentScrollTop > lastScrollTop.current + tolerance) {
+                // Scrolling down
+                setTopStyle('-10rem');
+            } else if (currentScrollTop < lastScrollTop.current - tolerance) {
+                // Scrolling up
+                setTopStyle('0rem');
+            }
+
+            lastScrollTop.current = currentScrollTop <= 0 ? 0 : currentScrollTop;
+        };
+
+        window.addEventListener('scroll', handleScroll);
+
+        return () => {
+            window.removeEventListener('scroll', handleScroll);
+        };
+    }, []);
+
     return (
         <>
-        <div className='topNavCont'>
-            <div className='logoCont'>
-            <img src='/images/noa/NOA3.svg' alt='logoSmall' className='topNavLogo' />
+            <div className='topNavCont' ref={navRef} style={{ top: topStyle }}>
+                <div className='logoCont'>
+                    <img src='/images/noa/NOA3.svg' alt='logoSmall' className='topNavLogo' />
+                </div>
+                <div className='topNavRight'>
+                    <MenuIcon className='menuIcon' onClick={handleOpen} />
+                </div>
             </div>
-            <div className='topNavRight'>
-                <MenuIcon className='menuIcon' onClick={handleOpen}/>
-            </div>
-        </div>
-        {
-            open ? <NavWindowMobile handleOpen={handleOpen} ref={navRef}/> : <></>
-        }
-        
+            {open && <NavWindowMobile handleOpen={handleOpen} ref={navRef} />}
         </>
-      
     );
-  }
-  
-  export default TopNav;
-  
+}
+
+export default TopNav;
