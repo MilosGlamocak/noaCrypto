4b2bb3ec2d280e74ab0b6d02baf1720d8e507925
MilosGlamocak <milosglamocak@gmail.com>
2024-09-18 12:56:06 +0200

    a

--- src/components/Background.jsx
+++ src/components/Background.jsx
@@ -1,25 +1,10 @@
-import React, { useEffect } from 'react';
-import '../styles/Background.scss';
+import React from 'react'
+import '../styles/Background.scss'
 
 function Background() {
-  useEffect(() => {
-    const setVh = () => {
-      const vh = window.innerHeight * 0.01;
-      document.documentElement.style.setProperty('--vh', `${vh}px`);
-    };
-
-    // Set initial vh value
-    setVh();
-
-    // Update on resize
-    window.addEventListener('resize', setVh);
-
-    return () => {
-      window.removeEventListener('resize', setVh);
-    };
-  }, []);
-
-  return <div className="bgCont"></div>;
+  return (
+    <div className='bgCont'></div>
+  )
 }
 
-export default Background;
+export default Background
