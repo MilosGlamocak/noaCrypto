4dae4dc0cf622cc16fa2f74e3d8d610741c53614
MilosGlamocak <milosglamocak@gmail.com>
2024-09-18 12:50:48 +0200

    a

--- src/components/Background.jsx
+++ src/components/Background.jsx
@@ -3,19 +3,17 @@ import '../styles/Background.scss';
 
 function Background() {
   useEffect(() => {
-    // Set the --vh custom property to the current window height
     const setVh = () => {
       const vh = window.innerHeight * 0.01;
       document.documentElement.style.setProperty('--vh', `${vh}px`);
     };
-    
-    // Initial setting of the vh
+
+    // Set initial vh value
     setVh();
-    
+
     // Update on resize
     window.addEventListener('resize', setVh);
-    
-    // Cleanup event listener on component unmount
+
     return () => {
       window.removeEventListener('resize', setVh);
     };
