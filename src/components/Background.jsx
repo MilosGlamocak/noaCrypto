91a793833581a29d57241270f76b9b7041e86df5
MilosGlamocak <milosglamocak@gmail.com>
2024-09-18 12:41:18 +0200

    a

--- src/components/Background.jsx
+++ src/components/Background.jsx
@@ -1,10 +1,27 @@
-import React from 'react'
-import '../styles/Background.scss'
+import React, { useEffect } from 'react';
+import '../styles/Background.scss';
 
 function Background() {
-  return (
-    <div className='bgCont'></div>
-  )
+  useEffect(() => {
+    // Set the --vh custom property to the current window height
+    const setVh = () => {
+      const vh = window.innerHeight * 0.01;
+      document.documentElement.style.setProperty('--vh', `${vh}px`);
+    };
+    
+    // Initial setting of the vh
+    setVh();
+    
+    // Update on resize
+    window.addEventListener('resize', setVh);
+    
+    // Cleanup event listener on component unmount
+    return () => {
+      window.removeEventListener('resize', setVh);
+    };
+  }, []);
+
+  return <div className="bgCont"></div>;
 }
 
-export default Background
+export default Background;
