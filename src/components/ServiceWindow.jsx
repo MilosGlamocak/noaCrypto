cf0eef5e3a41d59cf6e9a3d59b20d4a0d4c3e2f4
MilosGlamocak <milosglamocak@gmail.com>
2024-09-19 20:16:49 +0200

    load animation

--- src/components/ServiceWindow.jsx
+++ src/components/ServiceWindow.jsx
@@ -1,10 +1,34 @@
-import React from 'react'
+import React, { useEffect, useRef } from 'react';
 import '../styles/ServiceWindow.scss'
 
 
 function ServiceWindow({header, text, icon}) {
+
+  const serviceWindowRef = useRef(null);
+
+  useEffect(() => {
+    const observer = new IntersectionObserver((entries, observer) => {
+      entries.forEach(entry => {
+        if (entry.isIntersecting) {
+          entry.target.classList.add('animate');
+          observer.unobserve(entry.target); // Stop observing after animation
+        }
+      });
+    }, { threshold: 0.1 });
+
+    if (serviceWindowRef.current) {
+      observer.observe(serviceWindowRef.current);
+    }
+
+    return () => {
+      if (serviceWindowRef.current) {
+        observer.unobserve(serviceWindowRef.current);
+      }
+    };
+  }, []);
+
   return (
-    <div className='serviceWindowCont'>
+    <div className='serviceWindowCont' ref={serviceWindowRef}>
         <div className='icon'>
             {icon}
         </div>
