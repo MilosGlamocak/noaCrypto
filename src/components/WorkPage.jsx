c77c4fb9deb7a7276fab4be9e88af1b81b70afb0
MilosGlamocak <milosglamocak@gmail.com>
2024-09-19 15:28:35 +0200

    changed from png to jpg for faster load

--- src/components/WorkPage.jsx
+++ src/components/WorkPage.jsx
@@ -6,11 +6,11 @@ import WorkWindow from './WorkWindow'
 function WorkPage() {
 
     const work = [
-        {name: 'CATE', url: 'https://cateoneth.com/', img: '/images/cate.png'},
-        {name: '$WOOFIE', url: 'https://woofiesolana.com/', img: '/images/woofie.png'},
-        {name: '$BLOOP', url: 'https://bloopcoin.meme/', img: '/images/bloop.png'},
-        {name: '$BLUB', url: 'https://www.blubsui.com/', img: '/images/blub.png'},
-        {name: 'RA COIN', url: 'https://www.racoin.lol/', img: '/images/racoin.png'},
+        {name: 'CATE', url: 'https://cateoneth.com/', img: '/images/cate.jpg'},
+        {name: '$WOOFIE', url: 'https://woofiesolana.com/', img: '/images/woofie.jpg'},
+        {name: '$BLOOP', url: 'https://bloopcoin.meme/', img: '/images/bloop.jpg'},
+        {name: '$BLUB', url: 'https://www.blubsui.com/', img: '/images/blub.jpg'},
+        {name: 'RA COIN', url: 'https://www.racoin.lol/', img: '/images/racoin.jpg'},
     ]
 
   return (
