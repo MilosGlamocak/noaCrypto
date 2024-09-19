f286af9e80fa3f5aaf5d04b893eea67881b47823
MilosGlamocak <milosglamocak@gmail.com>
2024-09-19 22:48:11 +0200

    a

--- src/components/WelcomePage.jsx
+++ src/components/WelcomePage.jsx
@@ -4,7 +4,12 @@ import '../styles/WelcomePage.scss'
 function WelcomePage() {
   return (
     <div className='welcomePageCont'>
-        <h1>we value creativity.</h1>
+        {/*<h1>we value creativity.</h1>*/}
+        <img src="/images/noa/NOA3.svg" alt="logo" className='welcomeLogo'/>
+        <h1>The only marketing agency you will ever need.</h1>
+        <div className='bookBtn'>
+          <p>Book us now</p>
+        </div>
     </div>
   )
 }
